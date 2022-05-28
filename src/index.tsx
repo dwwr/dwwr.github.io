2a6dca3874dd1b519b9811685655ed62ffc1cdd0
dwwr <derek.warnerreyes@olioapps.com>
2022-05-28 10:16:28 -0700

    apply stylesheet, finish setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
+import './style.css'
 import App from './App'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+// ReactDOM.render(<App />, document.querySelector('#root'))
+
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(<App />)
