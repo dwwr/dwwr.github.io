cda938a0706a2160b9a9e96fa451893e6c928266
dwwr <derek.warnerreyes@olioapps.com>
2022-05-30 09:01:23 -0700

    Updates

--- src/modules/cubic_res/box_group/BoxGroup.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Group } from 'react-zdog'
-
-export const BoxGroup: React.FC<{ children: React.ReactElement }> = ({
-  children,
-}) => {
-  return <Group updateSort>{children}</Group>
-}
