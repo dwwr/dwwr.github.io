4d1fe5d95ed73e5ee1ebc0ed0f77107a2e0fe5b4
dwwr <derek.warnerreyes@olioapps.com>
2022-06-04 14:29:08 -0700

    devise face click detection scheme

--- src/scenes/CubeScene.tsx
+++ src/scenes/CubeScene.tsx
@@ -11,6 +11,11 @@ export interface CubeSceneProps {
   sideLength: number
 }
 
+const rgbToHex = (r: number, g: number, b: number) => {
+  if (r > 255 || g > 255 || b > 255) throw 'Invalid color component'
+  return ((r << 16) | (g << 8) | b).toString(16)
+}
+
 export const CubeScene: React.FC<CubeSceneProps> = ({
   contents,
   sideLength,
@@ -23,11 +28,45 @@ export const CubeScene: React.FC<CubeSceneProps> = ({
     top: contents.top.color,
     bottom: contents.bottom.color,
   }
+
+  const backwardColorMap: Record<string, string> = Object.fromEntries(
+    Object.entries(colorMap).map(([key, value]) => [value, key])
+  )
+
+  const getClickedColor = React.useCallback(() => {
+    const canvas = document.getElementsByTagName('canvas')[0]
+    canvas?.addEventListener(
+      'dblclick',
+      e => {
+        const rect = canvas.getBoundingClientRect()
+        const x =
+          ((e.clientX - rect.left) / (rect.right - rect.left)) * canvas.width
+        const y =
+          ((e.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height
+        var c = canvas.getContext('2d')
+        var p = c!.getImageData(x, y, 1, 1).data
+
+        var hex = '#' + ('000000' + rgbToHex(p[0], p[1], p[2])).slice(-6)
+        console.log(`%c${getClickedFace(hex)}`, `color: ${hex}`)
+      },
+      { once: true }
+    )
+  }, [])
+
+  const getClickedFace = (hex: string) => {
+    return backwardColorMap[hex]
+  }
+
   return (
     <Illustration
       rotate={{ x: (Zdog.TAU * -35) / 360, y: (Zdog.TAU * 1) / 8 }}
       element="canvas"
       dragRotate={true}
+      onDragStart={getClickedColor}
+      onDragEnd={() => {
+        const canvas = document.getElementsByTagName('canvas')[0]
+        canvas.ondblclick = null
+      }}
     >
       <BoxGroup>
         <ResumeBox sideLength={sideLength} colorMap={colorMap} />
