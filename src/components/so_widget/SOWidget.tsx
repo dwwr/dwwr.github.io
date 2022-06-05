81020cdfc1fec3664f5a140d87fa0306558e8556
dwwr <derek.warnerreyes@olioapps.com>
2022-06-05 12:51:25 -0700

    SOWidget

---
+++ src/components/so_widget/SOWidget.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const SOWidget: React.FC = () => {
+  return (
+    <a href="https://stackoverflow.com/users/15891793/sweetbabyd">
+      <img
+        src="https://stackoverflow.com/users/flair/15891793.png?theme=clean"
+        width="208"
+        height="58"
+        alt="profile for sweetBabyD at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
+        title="profile for sweetBabyD at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
+      />
+    </a>
+  )
+}
