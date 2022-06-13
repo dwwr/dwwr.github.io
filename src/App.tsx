d4619c9fb714ce55c2191c3bb8da2466028d8438
dwwr <derek.warnerreyes@olioapps.com>
2022-06-13 09:36:50 -0700

    wip widget switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,22 @@
 import React from 'react'
 import { CubeScene } from './scenes/CubeScene'
 import { contents, SIDE_LENGTH } from './content/contents'
-import { LIWidget } from './components/li_widget/LIWidget'
-import { GHWidget } from './components/gh_widget/GHWidget'
-import { SOWidget } from './components/so_widget/SOWidget'
-import { InstaWidget } from './components/insta_widget/InstaWidget'
-import { CashWidget } from './components/cash_widget/CashWidget'
 import { Face } from './content/types'
+import { CardContainer } from './components/card_container/CardContainer'
 
 const App: React.FC = () => {
-  const [currentFace, setCurrentFace] = React.useState<Face | 'none'>('none')
+  // const [currentFace, setCurrentFace] = React.useState<Face | 'none'>('none')
 
-  const widgetMap: Record<Face | 'none', () => React.ReactElement | null> = {
-    front: () => <LIWidget />,
-    right: () => <GHWidget />,
-    back: () => <SOWidget />,
-    left: () => <InstaWidget />,
-    top: () => <InstaWidget />,
-    bottom: () => <CashWidget />,
-    none: () => null,
-  }
-
-  const CurrentWidget = widgetMap[currentFace]
   return (
     <div className="app">
       <div className="cube-scene-container">
         <CubeScene
           contents={contents}
           sideLength={SIDE_LENGTH}
-          setCurrentFace={face => setCurrentFace(face)}
+          // setCurrentFace={face => {}}
         />
       </div>
-      <div className="card-container">
-        <CurrentWidget />
-      </div>
+      {/* <CardContainer currentFace={currentFace} /> */}
     </div>
   )
 }
--- src/scenes/CubeScene.tsx
+++ src/scenes/CubeScene.tsx
@@ -5,11 +5,12 @@ import { FacetAnchor } from '../modules/cubic_res/facet_anchor/FacetAnchor'
 import { BoxGroup } from '../modules/cubic_res/box_group/BoxGroup'
 import { ResumeBox } from '../modules/cubic_res/res_box/ResBox'
 import { Contents, Face } from '../content/types'
+import { CardContainer } from '../components/card_container/CardContainer'
 
 export interface CubeSceneProps {
   contents: Contents
   sideLength: number
-  setCurrentFace: (face: Face) => void
+  // setCurrentFace: (face: Face) => void
 }
 
 const rgbToHex = (r: number, g: number, b: number) => {
@@ -17,64 +18,67 @@ const rgbToHex = (r: number, g: number, b: number) => {
   return ((r << 16) | (g << 8) | b).toString(16)
 }
 
-export const CubeScene: React.FC<CubeSceneProps> = ({
-  contents,
-  sideLength,
-  setCurrentFace,
-}) => {
-  const colorMap: Record<Face, string> = {
-    front: contents.front.color,
-    right: contents.right.color,
-    back: contents.back.color,
-    left: contents.left.color,
-    top: contents.top.color,
-    bottom: contents.bottom.color,
-  }
+export const CubeScene = React.memo<CubeSceneProps>(
+  ({ contents, sideLength }) => {
+    const [currentFace, setCurrentFace] = React.useState<Face | 'none'>('none')
 
-  const backwardColorMap = Object.fromEntries(
-    Object.entries(colorMap).map(([key, value]) => [value, key])
-  ) as Record<string, Face>
+    const colorMap: Record<Face, string> = {
+      front: contents.front.color,
+      right: contents.right.color,
+      back: contents.back.color,
+      left: contents.left.color,
+      top: contents.top.color,
+      bottom: contents.bottom.color,
+    }
 
-  const getClickedColor = React.useCallback(() => {
-    const canvas = document.getElementsByTagName('canvas')[0]
-    canvas?.addEventListener(
-      'click',
-      e => {
-        const rect = canvas.getBoundingClientRect()
-        const x =
-          ((e.clientX - rect.left) / (rect.right - rect.left)) * canvas.width
-        const y =
-          ((e.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height
-        var c = canvas.getContext('2d')
-        var p = c!.getImageData(x, y, 1, 1).data
+    const backwardColorMap = Object.fromEntries(
+      Object.entries(colorMap).map(([key, value]) => [value, key])
+    ) as Record<string, Face>
 
-        var hex = '#' + ('000000' + rgbToHex(p[0], p[1], p[2])).slice(-6)
-        console.log(`%c${getClickedFace(hex)}`, `color: ${hex}`)
-      },
-      { once: true }
+    const getClickedColor = React.useCallback(() => {
+      const canvas = document.getElementsByTagName('canvas')[0]
+      canvas?.addEventListener(
+        'click',
+        e => {
+          const rect = canvas.getBoundingClientRect()
+          const x =
+            ((e.clientX - rect.left) / (rect.right - rect.left)) * canvas.width
+          const y =
+            ((e.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height
+          var c = canvas.getContext('2d')
+          var p = c!.getImageData(x, y, 1, 1).data
+
+          var hex = '#' + ('000000' + rgbToHex(p[0], p[1], p[2])).slice(-6)
+          console.log(`%c${getClickedFace(hex)}`, `color: ${hex}`)
+        },
+        { once: true }
+      )
+    }, [])
+
+    const getClickedFace = (hex: string) => {
+      setCurrentFace(backwardColorMap[hex] || 'none')
+      return backwardColorMap[hex]
+    }
+
+    return (
+      <>
+        <Illustration
+          rotate={{ x: (Zdog.TAU * -35) / 360, y: (Zdog.TAU * 1) / 8 }}
+          element="canvas"
+          dragRotate={true}
+          onDragStart={getClickedColor}
+          onDragEnd={() => {
+            const canvas = document.getElementsByTagName('canvas')[0]
+            canvas.onclick = null
+          }}
+        >
+          <BoxGroup>
+            <ResumeBox sideLength={sideLength} colorMap={colorMap} />
+            <FacetAnchor sideLength={sideLength} contents={contents} />
+          </BoxGroup>
+        </Illustration>
+        <CardContainer currentFace={currentFace} />
+      </>
     )
-  }, [])
-
-  const getClickedFace = (hex: string) => {
-    setCurrentFace(backwardColorMap[hex] || 'none')
-    return backwardColorMap[hex]
   }
-
-  return (
-    <Illustration
-      rotate={{ x: (Zdog.TAU * -35) / 360, y: (Zdog.TAU * 1) / 8 }}
-      element="canvas"
-      dragRotate={true}
-      onDragStart={getClickedColor}
-      onDragEnd={() => {
-        const canvas = document.getElementsByTagName('canvas')[0]
-        canvas.onclick = null
-      }}
-    >
-      <BoxGroup>
-        <ResumeBox sideLength={sideLength} colorMap={colorMap} />
-        <FacetAnchor sideLength={sideLength} contents={contents} />
-      </BoxGroup>
-    </Illustration>
-  )
-}
+)
