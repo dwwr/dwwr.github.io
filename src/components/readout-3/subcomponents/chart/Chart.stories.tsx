cf00c3be9c1e72275d3cb69bfebd5bd0da120c0a
dwwr <dwwarnerreyes@gmail.com>
2024-11-21 00:10:57 -0800

    mend chart story

--- src/components/readout-3/subcomponents/chart/Chart.stories.tsx
+++ src/components/readout-3/subcomponents/chart/Chart.stories.tsx
@@ -1,29 +1,42 @@
 /** @jsxImportSource react */
 import { Meta, StoryFn } from '@storybook/react'
-import Chart from './Chart'
+import Chart, { ChartProps } from './Chart'
 
 export default {
   title: 'Components/Readout3/Chart',
   component: Chart
 } as Meta
 
-const Template: StoryFn = () => (
+const Template: StoryFn<ChartProps> = ({ numberOfColumns, benchmark }) => (
   <div
     style={{
-      width: '100%',
+      width: '150%',
       height: '500px',
       backgroundColor: 'black',
       display: 'flex',
-      alignItems: 'start',
-      justifyContent: 'start',
+      // alignItems: 'start',
+      // justifyContent: 'start',
       boxSizing: 'border-box',
       overflow: 'hidden',
       padding: '1rem'
     }}
   >
-    <Chart numberOfColumns={20} />
+    <Chart numberOfColumns={numberOfColumns} benchmark={benchmark} />
   </div>
 )
 
-export const Default = Template.bind({})
-Default.args = {}
+export const Low = Template.bind({})
+Low.args = {
+  numberOfColumns: 40,
+  benchmark: 25
+}
+export const Medium = Template.bind({})
+Medium.args = {
+  numberOfColumns: 40,
+  benchmark: 50
+}
+export const High = Template.bind({})
+High.args = {
+  numberOfColumns: 40,
+  benchmark: 80
+}
