29a800ff97102774690aa3a9dc01c34c65e160ba
dwwr <dwwarnerreyes@gmail.com>
2024-12-07 11:55:29 -0800

    update border line style

--- src/components/readout-4/molecules/border-line/BorderLine.tsx
+++ src/components/readout-4/molecules/border-line/BorderLine.tsx
@@ -9,8 +9,8 @@ const borderLineStyles = css`
   position: relative;
   width: 100%;
   height: 4px;
-  background-color: #00ffff; // Adjust color to match your needs
-  margin: 1rem 0;
+  background-color: #00ffff;
+  margin: 1.5rem 0;
   filter: blur(1px);
 `
 
@@ -18,16 +18,17 @@ const labelStyles = css`
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%) scaleX(1.3);
+  transform: translate(-50%, -50%) scaleX(1.5);
   letter-spacing: 0.05rem;
   background-color: #000;
-  padding: 0 1rem;
+  padding: 0 1.25rem;
   color: #00ffff;
-  font-family: 'Helvetica Neue', sans-serif;
+  font-size: clamp(1.4rem, 2vw, 1.4rem);
+  font-family: 'Helvetica';
   font-weight: bold;
   text-transform: uppercase;
   border: 2px solid #00ffff;
-  border-radius: 4px;
+  border-radius: 6px;
 `
 
 export const BorderLine: React.FC<BorderLineProps> = ({ text }) => {
