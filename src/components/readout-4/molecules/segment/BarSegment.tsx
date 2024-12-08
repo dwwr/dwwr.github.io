40cbef767761f9dd4f0cb6e53f2b6f420d21a2c2
dwwr <dwwarnerreyes@gmail.com>
2024-12-07 23:52:31 -0800

    more cleanup

--- src/components/readout-4/molecules/segment/BarSegment.tsx
+++ src/components/readout-4/molecules/segment/BarSegment.tsx
@@ -6,6 +6,7 @@ const barSegmentFlicker = css`
   animation: ${flickerAnimation} 0.1s infinite;
 `
 const greenRGB = '139, 232, 186'
+const redRGB = '216, 121, 57'
 
 const greenText = css`
   color: rgba(${greenRGB}, 0.95);
@@ -13,10 +14,22 @@ const greenText = css`
     0 0 10px rgba(${greenRGB}, 0.95);
 `
 
+const redText = css`
+  color: rgba(${redRGB}, 0.95);
+  text-shadow: 0 0 10px rgba(${redRGB}, 0.95), 0 0 10px rgba(${redRGB}, 0.95),
+    0 0 10px rgba(${redRGB}, 0.95);
+`
+
 const greenBar = css`
   background-color: rgba(${greenRGB}, 0.95);
   box-shadow: 0 0 10px rgba(${greenRGB}, 0.4), 0 0 10px rgba(${greenRGB}, 0.3),
     0 0 10px rgba(${greenRGB}, 0.2), 0 0 10px rgba(${greenRGB}, 0.1);
+`
+
+const redBar = css`
+  background-color: rgba(${redRGB}, 0.95);
+  box-shadow: 0 0 10px rgba(214, 63, 43, 0.4), 0 0 10px rgba(214, 63, 43, 0.3),
+    0 0 10px rgba(201, 43, 22, 0.2), 0 0 10px rgba(201, 43, 22, 0.1);
 `
 
 const container = (flicker?: boolean) => css`
@@ -34,23 +47,17 @@ const barContainer = css`
 
 const bar = (green?: boolean) => css`
   ${barContainer}
-  background-color: rgb(216, 121, 57);
-  box-shadow: 0 0 10px rgba(214, 63, 43, 0.4), 0 0 10px rgba(214, 63, 43, 0.3),
-    0 0 10px rgba(201, 43, 22, 0.2), 0 0 10px rgba(201, 43, 22, 0.1);
-  ${green && greenBar}
+  ${green ? greenBar : redBar}
 `
 
 const glowText = (green?: boolean) => css`
   font-size: clamp(0.8rem, 2vw, 1.1rem);
   font-family: 'Helvetica', monospace;
   font-weight: bold;
-  /* color: rgba(196, 106, 65, 0.8); */
-  color: rgb(216, 121, 57);
-  text-shadow: 0 0 10px #ff3300, 0 0 10px #ff3300, 0 0 10px #ff3300;
   text-transform: uppercase;
   line-height: 1.1;
   white-space: nowrap;
-  ${green && greenText}
+  ${green ? greenText : redText}
 `
 
 const numberText = (green?: boolean) => css`
